a70c534eaec565e8411b5a222465b817a5d21cbb
漂泊80 <13671172337@163.com>
2017-11-16 18:07:03 +0800

    查看个人信息头像大图

---
+++ PoemRN/app/ui/PhotoUI.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View ,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import{
+  PImage,
+  Utils,
+}from '../AppUtil';
+const {width, height} = Dimensions.get('window');
+export default class PhotoUI extends React.Component{
+  static navigationOptions = ({navigation}) => ({
+    header:null,
+  });
+  constructor(props){
+    super(props);
+    let params = this.props.navigation.state.params;
+    let photo = params.photo;
+    console.log('-----PhotoUI() photo:',photo);
+    this.state = {
+      photo:photo,
+    }
+  }
+  render(){
+    return(
+      <TouchableOpacity style={styles.container}
+        onPress={()=>{
+          const { goBack } = this.props.navigation;
+          goBack();
+        }}
+        >
+          <PImage
+            style={styles.photo}
+            source={this.state.photo}
+          />
+      </TouchableOpacity>
+    )
+  }
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#00000088',
+      alignItems:'center',
+      justifyContent:'center',
+    },
+    photo:{
+      backgroundColor: '#ffffff',
+      width:width,
+      height:width,
+    },
+})
