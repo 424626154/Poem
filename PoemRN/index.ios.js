ddc8d1b42b615a8692a6dfc5a8a10edccd8e1707
bb s <424626154@qq.com>
2017-12-01 00:09:30 +0800

    修改Android配置

--- PoemRN/index.ios.js
+++
@@ -1,5 +0,0 @@
-import {
-  AppRegistry,
-} from 'react-native';
-import App from './app/App';
-AppRegistry.registerComponent('PoemRN', () => App);
--- PoemRN/index.js
+++ PoemRN/index.js
@@ -1,5 +1,5 @@
 import {
-  AppRegistry,
-} from 'react-native';
+    AppRegistry
+  } from 'react-native';
 import App from './app/App';
 AppRegistry.registerComponent('PoemRN', () => App);
